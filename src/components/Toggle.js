bb8e628bb3686b043dd1e1806acc23ea47cb1c7a
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-08-01 22:01:13 +0300

    adds a toggle on the button

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+function Toggle (){
+ const [isOn, setIsOn] = useState(true)
+
+ function handleClick(e){
+  setIsOn((isOn)=>!isOn)
+ }
+ const colors = isOn ? "white" : "red"
+
+  return (
+    <>
+      <button style={{background:colors}} onClick={handleClick}>{isOn ? "ON" :"OFF"}</button>
+    </>
+  )
 }
 
 export default Toggle;
